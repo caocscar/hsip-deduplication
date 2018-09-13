d2bbeb4cfd5b01521e3021e39dece5aee872e6e6
unknown <caoa@CSCAR-2015-VFC.adsroot.itcs.umich.edu>
2018-09-13 13:04:25 -0400

    updated algorithm with 4 rds of blocking

--- hsip_alexid.py
+++ hsip_alexid.py
@@ -21,6 +21,7 @@ parser.add_argument('-f','--filename', type=str, help='name of Excel file')
 parser.add_argument('-t','--threshold', type=float, default=0.85, help='threshold to use for string match using Jaro-Winkler distance')
 args = parser.parse_args()
 args.filename = '1 HSIP_Data_File-December_Copy.xlsx'
+threshold = args.threshold
 
 if not args.filename:
     print("You need to supply a --filename argument")
@@ -35,6 +36,11 @@ df_raw.columns = ['hsip', 'sid', 'name', 'email', 'ssn',
 rules = pd.read_csv('rules.txt', sep='|')
    
 #%% Filter dataset based on rules.txt
+ssn_list = list(rules.loc[rules['column'] == 'ssn','value'])
+ssn_dict = {}
+for ssn in ssn_list:
+    ssn_dict[ssn] = None
+
 rules_dict = defaultdict(list)
 for col, rule in rules.groupby('column'):
     rules_dict[col] = rule['value'].tolist()
@@ -63,8 +69,7 @@ keep_rows = score['total'] >= 2
 df = df_raw[keep_rows].reset_index(drop=True)
 
 # data cleanup
-df = df[df['amt'] > 0]
-df['address_1'] = df['address_1'].str.replace(' ','').str.lower()
+df['address_'] = df['address_1'].str.replace(' ','').str.lower()
 df['name'] = df['name'].str.lower()
 df['n'] = df['name'].apply(lambda x: len(x.split()) )
 names = df['name'].str.split(' ', expand=True, n=2)
@@ -77,36 +82,52 @@ df2 = df[tf]
 df2.rename(columns={'last':'middle','middle':'last'}, inplace=True)
 df = pd.concat([df1, df2], sort=False)
 
-#%% blocking
-indexer = rl.BlockIndex(on='last')
-pairs = indexer.index(df)
-print('Pairs to compare = {:,}'.format(len(pairs)) )
-
-#%% Set rules for matching
-threshold = args.threshold
-rules = rl.Compare()
-rules.exact('last', 'last', label='last_name')
-rules.string('first', 'first', label='first_name', method='jarowinkler', threshold=threshold)
-rules.string('ssn', 'ssn', label='ssn', method='jarowinkler', threshold=threshold)
-rules.string('address_1', 'address_1', label='address', threshold=threshold)
-
-t1 = time.time()
-computation = rules.compute(pairs, df)
-t2 = time.time()
-print('Matching took {:.1f} mins'.format((t2-t1)/60) )
-print('Pairs per second: {:.0f}'.format(len(pairs)/(t2-t1)))
-features = computation.copy()
+#%% create dataframe for linking
+df['initials'] = df['first'].str[0] + df['last'].str[0]
+df_linkage = df[['first','last','initials','ssn','address_']]
+df_linkage.replace({'ssn':ssn_dict}, inplace=True)
 
 #%%
-features['name'] = 0.5*features['first_name'] + 0.5*features['last_name']
-features.drop(['first_name','last_name'], axis=1, inplace=True)
-features['score'] = features.sum(axis=1)
-features.reset_index(inplace=True)
-features = features.astype(int)
-if 'level_0' in features.columns:
-    features.rename(columns={'level_0':'rec1','level_1':'rec2'}, inplace=True)
-features.sort_values(['score','rec1','rec2'], ascending=[False,True,True], inplace=True)
-matches = features[features['score'] >= 2].reset_index(drop=True)
+def get_index_pairs_rules(df, columns):
+    if isinstance(columns, str):
+        columns = [columns]
+    rules = rl.Compare()
+    rules.string('first', 'first', label='first', method='jarowinkler', threshold=threshold)
+    rules.string('last', 'last', label='last', method='jarowinkler', threshold=threshold)
+    rules.string('ssn', 'ssn', label='ssn', method='jarowinkler', threshold=threshold)
+    rules.string('address_', 'address_', label='address_', method='jarowinkler', threshold=threshold)
+    for col in columns:
+        rules.exact(col, col, label=col)
+    indexer = rl.BlockIndex(on=columns)
+    idx_pairs = indexer.index(df)
+    return idx_pairs, rules
+
+def score_calculation(df):    
+    df['name'] = 0.5*df['first'] + 0.5*df['last']
+    df = df[['name','ssn','address_']]       
+    df['total'] = df.sum(axis=1)
+    df.reset_index(inplace=True)
+    df.rename(columns={'level_0':'rec1','level_1':'rec2'}, inplace=True)
+    df.sort_values(['total','rec1','rec2'], ascending=[False,True,True], inplace=True)
+    return df
+
+#%% blocking and linking
+blocks = ['ssn','address_',['last','initials'],['first','initials']]
+pair_score = []
+for block in blocks:
+    idx_pairs, rules = get_index_pairs_rules(df_linkage, block)
+    print('{} blocking - Pairs to compare: {:,}'.format(block, len(idx_pairs)) )
+    t1 = time.time()
+    computation = rules.compute(idx_pairs, df_linkage)
+    t2 = time.time()
+    print('Matching took {:.1f} sec'.format(t2-t1) )
+    print('Pairs per second: {:.0f}'.format(len(idx_pairs)/(t2-t1)))
+    features = computation.copy()
+    pair_score.append(score_calculation(features))
+
+scores = pd.concat(pair_score, ignore_index=True, sort=False)
+matches = scores[scores['total'] >= 2]
+matches.reset_index(inplace=True)
 
 #%% assigns id to each person based on match
 labels = {}
@@ -120,63 +141,63 @@ for i in range(matches.shape[0]):
         sid += 1
 pid = pd.DataFrame.from_dict(labels, orient='index')
 pid.columns = ['alexid']
+maxid = sid + 1
 
 #%%
 dakota = df.merge(pid, how='left', left_index=True, right_index=True)
-maxid = int(dakota['alexid'].max())
-dupes = dakota[dakota['alexid'].notnull()]
+alex = dakota[dakota['alexid'].notnull()]
 singletons = dakota[dakota['alexid'].isnull()]
 singletons.dropna(axis=1, how='all', inplace=True)
 
-#%% Re-block on first name
-indexer2 = rl.BlockIndex(on='first')
-pairs2 = indexer.index(singletons)
-print('Pairs to compare = {:,}'.format(len(pairs2)) )
-
-#%%
-rules2 = rl.Compare()
-rules2.string('last', 'last', label='last_name', method='jarowinkler', threshold=threshold)
-rules2.exact('first', 'first', label='first_name')
-rules2.string('ssn', 'ssn', label='ssn', method='jarowinkler', threshold=threshold)
-rules2.string('address_1', 'address_1', label='address', threshold=threshold)
-
-t1 = time.time()
-computation2 = rules2.compute(pairs2, singletons)
-t2 = time.time()
-print('Matching took {:.1f} mins'.format((t2-t1)/60) )
-print('Pairs per second: {:.0f}'.format(len(pairs2)/(t2-t1)))
-features2 = computation2.copy()
-
-#%% Make sure there are no matches; otherwise more code is needed
-features2['name'] = 0.5*features2['first_name'] + 0.5*features2['last_name']
-features2.drop(['first_name','last_name'], axis=1, inplace=True)
-features2['score'] = features2.sum(axis=1)
-assert features2['score'].max() < 2
-
-#%%
-singletons['alexid'] = range(singletons.shape[0])
-singletons['alexid'] += maxid + 1
-
-lily = pd.concat([dupes, singletons])
+singletons['alexid'] = range(maxid, maxid+singletons.shape[0])
+lily = pd.concat([alex, singletons])
 lily['alexid'] = lily['alexid'].astype(int)
 lily.sort_values('alexid', inplace=True)
 
-#%%
 ppl = lily.groupby('alexid')['amt'].sum().to_frame()
 ppl.columns = ['total']
 master = lily.merge(ppl, left_on='alexid', right_index=True)
 master.sort_values(['total','alexid'], ascending=[False,True], inplace=True)
-nodupes = master.drop_duplicates(['alexid','total'])
+nodupes = master.drop_duplicates(['alexid'])
 f1099 = nodupes[nodupes['total'] > 600]
 
 #%%
 outputfile = filename.replace('.xlsx','_alexid.xlsx')
 writer = pd.ExcelWriter(outputfile)
-xlsx = master.drop(['n','first','middle','last'], axis=1)
+xlsx = master.drop(['address_','n','first','middle','last','initials'], axis=1)
 xlsx.to_excel(writer, 'alexid', index=False)
 df_raw[~keep_rows].to_excel(writer, 'invalid_rows', index=False)
 writer.save()
 print('{} created'.format(outputfile))
 
+#%%
+kathy = pd.read_excel('2nd Run Differences.xlsx', sheet_name='Dec Rollup')
+kathy['key'] = kathy['HSIP Control No'].astype(str) + '_' + kathy['Subject#'].astype(str)
+kathy.sort_values('key', inplace=True)
+kathy.reset_index(drop=True, inplace=True)
+knodupes = kathy.drop_duplicates('Formatted SSN')
+kset = set(knodupes['key'])
 
+kmaster = master.copy()
+kmaster['key'] = kmaster['hsip'].astype(str) + '_' + kmaster['sid'].astype(str)
+kmaster.sort_values('key', inplace=True)
+kmaster.reset_index(drop=True, inplace=True)
+nodupes_ = kmaster.drop_duplicates(['alexid'])
+aset = set(nodupes_['key'])
 
+diffset1 = aset.difference(kset)
+diffset2 = kset.difference(aset)
+
+#%%
+cfile = 'differences.xlsx'
+writer = pd.ExcelWriter(cfile)
+xlsx = nodupes_.drop(['address_','n','first','middle','last','initials'], axis=1)
+xlsx = xlsx[xlsx['key'].isin(diffset1)]
+xlsx.sort_values('ssn', ascending=False, inplace=True)
+xlsx.to_excel(writer, 'alex diff kathy', index=False)
+xlsx2 = knodupes[knodupes['key'].isin(diffset2)]
+xlsx2 = xlsx2.merge(kmaster[['key','alexid']], how='left', on='key')
+xlsx2.sort_values('alexid', inplace=True)
+xlsx2.to_excel(writer, 'kathy diff alex', index=False)
+writer.save()
+print('{} created'.format(cfile))
